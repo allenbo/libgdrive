63719d8ed2d7242cef308fccc1ce81116ce79395
abo <linjianfengqrh@gmail.com>
2014-08-05 17:14:43 -0700

    Add missing file

---
+++ include/gdrive/gdrive.hpp
@@ -0,0 +1,15 @@
+#ifndef __GDRIVE_GDRIVE_HPP__
+#define __GDRIVE_GDRIVE_HPP__
+
+
+#include "gdrive/aboutservice.hpp"
+#include "gdrive/changeservice.hpp"
+#include "gdrive/credential.hpp"
+#include "gdrive/drive.hpp"
+#include "gdrive/filecontent.hpp"
+#include "gdrive/gitem.hpp"
+#include "gdrive/oauth.hpp"
+#include "gdrive/servicerequest.hpp"
+#include "gdrive/store.hpp"
+
+#endif
